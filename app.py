363514e04bd70b1d0053df7e8d79c222cdef3ce7
Fabio Jun <fabiojundev@gmail.com>
2022-06-07 16:06:18 -0300

    Add delete post feature

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def get_post(post_id):
     conn = get_db_connection()
@@ -16,8 +18,10 @@ def get_post(post_id):
         abort(404)
     return post
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'suuuuurfing in indonesia'
+
 
 @app.route('/')
 def index():
@@ -31,6 +35,7 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
@@ -49,6 +54,7 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
@@ -70,3 +76,14 @@ def edit(id):
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
